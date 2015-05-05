1c1ec2635fca246acb23d8870377acc6d3de2e5a
Jim McAndrew <jim@loc8.us>
2015-05-05 10:54:14 -0600

    allow for a payload

--- src/getOsmData.js
+++ src/getOsmData.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var osmtogeojson = require('osmtogeojson');
 var Stream = require('stream');
 
-var pathToStream = function(osmPath) {
+var pathToStream = function(osmPath, payload) {
   var type, dataStream = [],
     bounds = {};
   if (typeof osmPath === 'object') {
@@ -25,7 +25,20 @@ var pathToStream = function(osmPath) {
   }
   type = osmPath.substr(0, 4);
   if (type === 'http' || type === 'ftp:') {
-    dataStream = request(osmPath);
+    if (payload) {
+      dataStream = new Stream.Readable();
+      dataStream._read = function noop() {};
+      request.post({
+        'url': osmPath,
+        'method': 'POST',
+        'body': payload
+      }, function(e, r, b) {
+        dataStream.push(b);
+        dataStream.push(null);
+      });
+    } else {
+      dataStream = request(osmPath);
+    }
   } else if (type === '<xml' || type === '<?xm') {
     dataStream = new Stream.Readable();
     dataStream._read = function noop() {};
@@ -37,23 +50,29 @@ var pathToStream = function(osmPath) {
   return dataStream;
 };
 
-module.exports = function(osmPath, callback) {
-  var dataStream = [], osmxmlParser;
-  return new(datawrap.mockingbird(callback))(function(fulfill, reject) {
-    osmxmlParser = new OsmxmlParser();
-    dataStream = pathToStream(osmPath);
+module.exports = function(osmPath, payload) {
+  var dataStream = [],
+    osmxmlParser;
+  return new datawrap.Bluebird(function(fulfill, reject) {
+    // If we're passed pre-pared OSM data, just send it back
+    if (typeof osmPath === 'object' && osmPath.type === 'FeatureCollection') {
+      fulfill(osmPath);
+    } else {
+      osmxmlParser = new OsmxmlParser();
+      dataStream = pathToStream(osmPath, payload);
 
-    dataStream
-      .on('data', function(chunk) {
-        osmxmlParser.write(chunk);
-      })
-      .on('error', function(e) {
-        reject(e);
-      })
-      .on('end', function() {
-        osmxmlParser.end();
-        fulfill(osmtogeojson(osmxmlParser.getJSON()));
-      });
-    dataStream.resume();
+      dataStream
+        .on('data', function(chunk) {
+          osmxmlParser.write(chunk);
+        })
+        .on('error', function(e) {
+          reject(e);
+        })
+        .on('end', function() {
+          osmxmlParser.end();
+          fulfill(osmtogeojson(osmxmlParser.getJSON()));
+        });
+      dataStream.resume();
+    }
   });
 };
