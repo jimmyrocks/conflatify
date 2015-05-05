40bb1853e8bf867f0c7b663210d9caff65a53662
Jim McAndrew <jim@loc8.us>
2015-05-05 13:37:14 -0600

    Add a 15 meter buffer to the get bounds function

--- sql/getTagBounds.sql
+++ sql/getTagBounds.sql
@@ -1,14 +1,20 @@
 SELECT
-  "tags"."k",
-  "tags"."v",
-  MbrMinX(ST_Union("the_geom")) AS "left",
-  MbrMinY(ST_Union("the_geom")) AS "bottom",
-  MbrMaxX(ST_Union("the_geom")) AS "right",
-  MbrMaxY(ST_Union("the_geom")) AS "top"
-FROM
-  {{tagTableName}} "tags" JOIN {{tableName}} "geoms" ON "tags"."osmid" = "geoms"."osmid"
-WHERE
-  "tags"."interesting" = 't'
-GROUP BY
-  "k",
-  "v";
+ "matched_tags"."k" AS "k",
+ "matched_tags"."v" AS "v",
+  MbrMinX("matched_tags"."the_geom") AS "left",
+  MbrMinY("matched_tags"."the_geom") AS "bottom",
+  MbrMaxX("matched_tags"."the_geom") AS "right",
+  MbrMaxY("matched_tags"."the_geom") AS "top"
+FROM (
+  SELECT
+    "tags"."k",
+    "tags"."v",
+    Transform(Buffer(Transform(ST_Union("geoms"."the_geom"),3857),15),4326) AS "the_geom"
+  FROM
+    "{{tagTableName}}" "tags" JOIN "{{tableName}}" "geoms" ON "tags"."osmid" = "geoms"."osmid"
+  WHERE
+    "tags"."interesting" = 't'
+  GROUP BY
+    "k",
+    "v"
+) "matched_tags";
