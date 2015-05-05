103294eeb9cddf89aba8a9501431e1b4036ac22d
Jim McAndrew <jim@loc8.us>
2015-05-05 13:54:41 -0600

    Quote the table names, we never know when one will have a space in it

--- sql/updateInterestingTags.sql
+++ sql/updateInterestingTags.sql
@@ -1,16 +1,12 @@
 UPDATE
-  {{tagTableName}}
+  "{{tagTableName}}"
 SET
   "interesting" = 't'
 WHERE
   "k"='{{k}}' AND
   ("v"='{{v}}' OR '{{v}}' = '*'); AND
   (
-    SELECT GeometryType(the_geom)
-   /*   CASE
-        WHEN "type" = 'n' THEN 'n'
-        ELSE 'w'
-      END AS "type" */
+    SELECT GeometryType("the_geom")
     FROM "{{tableName}}"
     WHERE "{{tableName}}"."osmid" = "{{tagTableName}}"."osmid"
   ) LIKE '%{{geometryType}}';
