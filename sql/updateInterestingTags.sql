8b29e9ffdb6ea39142efc3abeab51e53a133c7bf
Jim McAndrew <jim@loc8.us>
2015-05-05 13:37:28 -0600

    Include '*' tags

--- sql/updateInterestingTags.sql
+++ sql/updateInterestingTags.sql
@@ -4,9 +4,14 @@ SET
   "interesting" = 't'
 WHERE
   "k"='{{k}}' AND
-  "v"='{{v}}' AND
+  ("v"='{{v}}' OR '{{v}}' = '*'); AND
   (
     SELECT GeometryType(the_geom)
-    FROM {{tableName}}
-    WHERE {{tableName}}.osmid = {{tagTableName}}.osmid
+   /*   CASE
+        WHEN "type" = 'n' THEN 'n'
+        ELSE 'w'
+      END AS "type" */
+    FROM "{{tableName}}"
+    WHERE "{{tableName}}"."osmid" = "{{tagTableName}}"."osmid"
   ) LIKE '%{{geometryType}}';
+
