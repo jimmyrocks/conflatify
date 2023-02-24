d01ba42ff436b8041fb6ae6d5bcfdbd0f9c0065c
Jim McAndrew <jim@loc8.us>
2023-02-23 21:28:17 -0500

    Update paths .oql → .overpassql
    https://github.com/jimmyrocks/conflatify/issues/1

--- src/getTagBoundsFromOverpass.js
+++ src/getTagBoundsFromOverpass.js
@@ -6,9 +6,9 @@ var buildOverpassQuery = function(tagPairBounds) {
   var requestList = '';
   tagPairBounds.map(function(tagPair) {
     tagPair.tagPair = '"' + tagPair.k + '"' + (tagPair.v === '*' ? '' : '="' + tagPair.v + '"');
-    requestList += '\n' + fandlebars(fs.readFileSync('./sql/overpass-requests.oql', 'utf8'), tagPair);
+    requestList += '\n' + fandlebars(fs.readFileSync('./sql/overpass-requests.overpassql', 'utf8'), tagPair);
   });
-  return fandlebars(fs.readFileSync('./sql/overpass-query.oql', 'utf8'), {
+  return fandlebars(fs.readFileSync('./sql/overpass-query.overpassql', 'utf8'), {
     requestList: requestList
   });
 };
